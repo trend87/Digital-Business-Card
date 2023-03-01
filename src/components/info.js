e3a7fb2d03c5d85b7b415c8665f157f9632c1042
Effiong Israel Robert <israelrobert@gmail.com>
2023-03-01 23:39:34 +0100

    renamed components

---
+++ src/components/info.js
@@ -0,0 +1,13 @@
+import React from "react"
+import profileImage from "../images/library1.jpg"
+
+function Info() {
+    return(
+        <div className="image">
+            <img src={profileImage} className="profile-image"/>
+        </div>
+    )
+}
+
+
+export default Info
