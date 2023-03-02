b440d2d5b84215928c8188c701118f4f5d01b5a7
Effiong Israel Robert <israelrobert@gmail.com>
2023-03-02 11:04:26 +0100

    add social links to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,17 @@
 import React from "react"
+import github from "../images/GitHub Icon.png"
+import twitter from "../images/Twitter Icon.png"
+import facebook from "../images/Facebook Icon.png"
 
 function Footer() {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className="footer">
+            <a href="https://twitter.com/EffiongIsrael8"><img src={twitter} /></a>
+            <a href="https://web.facebook.com/israel.effiongrobert"><img src={facebook} /></a>
+            <a href="https://github.com/trend87"> <img src={github} /></a>
+            
+           
+
         </div>
     )
 }
