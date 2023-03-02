29e5ea9740ec3b86edd1588a192d870c0c489e8a
Effiong Israel Robert <israelrobert@gmail.com>
2023-03-02 09:51:17 +0100

    add icons to button and style

--- src/components/info.js
+++ src/components/info.js
@@ -1,10 +1,26 @@
 import React from "react"
 import profileImage from "../images/library1.jpg"
+import mail from "../images/mail.png"
+import linkedin from "../images/linkedin.png"
 
 function Info() {
     return(
-        <div className="image">
+        <div className="image-container">
             <img src={profileImage} className="profile-image"/>
+            
+            <div className="image-content">
+                
+                <h3>Israel E. Robert</h3>
+                <p>Frontend Developer</p>
+               
+                
+            </div>
+
+            <div className="info-buttons">
+                <button className="mail-button"> <img src={mail}/> Email</button>
+                <button className="link-button"> <img src={linkedin} /> LinkedIn</button>
+            </div>
+            
         </div>
     )
 }
